a00946c19d437c2078c53e8d4a68b12b9184a359
125102072-del <125102072@umail.ucc.ie>
2025-10-08 18:51:37 +0100

    Project Scaffolding - React
    Initial Project Scaffolding has been done for React

---
+++ React/CineHub/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header style={{ background: "#282c34", padding: "10px", color: "white" }}>
+      <h1>Welcome to My React App</h1>
+    </header>
+  );
+}
+
+export default Header;
